d36a2a2cb7ab1e7e98e80bf1fd10bd8a0cdf2463
Patrick van der Linden <patrick@weareblis.com>
2015-05-08 23:20:55 +0200

    Updated configuration seed.

--- Zaggoware.BugTracker.Data/Migrations/Configuration.cs
+++ Zaggoware.BugTracker.Data/Migrations/Configuration.cs
@@ -32,19 +32,57 @@ namespace Zaggoware.BugTracker.Data.Migrations
             //    );
             //
 
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+            }
+
             if (
                 context.Users.SingleOrDefault(
                     u => u.UserName.Equals("Admin", StringComparison.InvariantCultureIgnoreCase)) == null)
             {
-                var userManager = new UserManager<User>(new UserStore<User>(context));
+                var userManager = this.ResolveUserManager(context);
                 var admin = context.Users.Create();
                 admin.UserName = "Admin";
+                admin.Email = "admin@zaggoware.nl";
+                admin.EmailConfirmed = true;
+                admin.PasswordHash = userManager.PasswordHasher.HashPassword("Admin");
 
                 userManager.Create(admin, "Admin");
 
                 context.Users.Add(admin);
                 context.SaveChanges();
+
+                userManager.AddToRole(admin.Id, "Admin");
             }
+        }
+
+        private UserManager<User> ResolveUserManager(Data.DbContext context)
+        {
+            var userStore = new UserStore<User>(context);
+            var userManager = new UserManager<User>(userStore);
+
+            // Configure validation logic for usernames
+            userManager.UserValidator = new UserValidator<User>(userManager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+
+            // Configure validation logic for passwords
+            userManager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 8,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
+            };
+
+            return userManager;
         }
     }
 }
